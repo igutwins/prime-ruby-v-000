da81a2742b6ab27f489ab2a497e01780e625fe02
Ian Gutwinski <iangutwinski@gmail.com>
2019-02-02 21:02:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(integer)
+  if integer == 2
+    true
+  else
+  i=2
+  while i < integer
+    if integer % i == 0
+      false
+    else
+      true
+  i+=1
+  end
+end
+end
+end 
